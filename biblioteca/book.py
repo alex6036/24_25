d88386d24045e98e85abe0e55cb42a10446b110d
alex6036 <145665271+alex6036@users.noreply.github.com>
2025-04-30 08:12:40 +0000

    book 2

--- biblioteca/book.py
+++ biblioteca/book.py
@@ -6,3 +6,28 @@ class Book:
         self._author = author
         self._genre = genre
         self._is_available = True  # Por defecto, el libro está disponible
+    # Getters
+    def get_title(self):
+        return self._title
+
+    def get_author(self):
+        return self._author
+
+    def get_genre(self):
+        return self._genre
+
+    def is_available(self):
+        return self._is_available
+
+    # Setters
+    def set_title(self, title):
+        self._title = title
+
+    def set_author(self, author):
+        self._author = author
+
+    def set_genre(self, genre: BookGenre):
+        self._genre = genre
+
+    def set_availability(self, status: bool):
+        self._is_available = status
